f8311b3260d787c4225adff07af7d117d6ddcde9
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-08-07 15:45:03 -0300

    fix

--- src/schemas/headersSchema.js
+++ src/schemas/headersSchema.js
@@ -1,5 +1,5 @@
 import joi from "joi";
 
 export const headersSchema = joi.object({
-    Authorization: joi.string().required().pattern(/Bearer /)
+    Authorization: joi.string().required().pattern(/Bearer /) 
 });
